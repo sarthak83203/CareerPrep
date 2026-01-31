426da859a0f30522c916e0721d56160c42caa1a7
sarthak <sarthak.shinde2006@gmail.com>
2026-02-01 01:06:30 +0530

    Login page setup

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,6 +1,34 @@
-export default function Login(){
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login({setCurrentPage}){ //props
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [error,setError]=useState(null);
+    const navigate=useNavigate();
+    
+    const handleLogin=async (e)=>{
+        e.preventDefault();
+
+    }
     return(
-        <div></div>
+        <div className="w-[90vw] md:w-[33vw] p-7  flex flex-col justify-center">
+            <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+            <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter you detail to log in</p>
+            <form onSubmit={handleLogin}>
+                
+
+               
+
+
+
+
+
+
+
+            </form>
+
+        </div>
 
     );
 }
